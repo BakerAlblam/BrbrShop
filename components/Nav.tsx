99f305d58a8649fc1a76908e4c746fa3bd8306f6
BakerAlblam <gallardo0@outlook.com>
2024-02-10 18:28:45 +0100

    home

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -12,7 +12,6 @@ const Nav = () => {
           href="#"
         >
           <HomeIcon className="w-5 h-5 mr-2" />
-          Home
         </Link>
       </nav>
       <div className="flex items-center gap-4 md:gap-6">
